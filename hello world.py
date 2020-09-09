38e4855be40a9eaa29bd886c801f779906e21787
Vaultech101 <70965926+Vaultech101@users.noreply.github.com>
2020-09-09 13:40:31 -0600

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -2,4 +2,3 @@ Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit 
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("tis but a flesh wound")
 tis but a flesh wound
->>> 
